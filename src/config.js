06f760751c8e3b98152bdc04f800d35154267872
Enijar <TheEnijar@gmail.com>
2021-10-19 17:09:03 +0100

    feat: slide indices 0 and 1 UI

--- src/config.js
+++ src/config.js
@@ -1,11 +1,42 @@
 module.exports = {
   jobCode: `EVZ_2021_0060_UK <br/>OCTOBER 2021`,
-  disclaimer: ``,
-  frames: [
+  disclaimer: `
+    For Healthcare Professionals Only<br/>
+    <span class="highlight-primary">Click here for UK Prescribing Information and Adverse Event Reporting</span>
+  `,
+  slides: [
     {
-      prelude: ``,
-      title: ``,
-      background: ``,
+      content: `
+        <h2 class="prelude">EVRENZO (roxadustat) is indicated for treatment of adult <br/>patients with symptomatic anaemia associated with CKD<sup>1</sup></h2>
+        <h1 class="title">In anaemia of chronic kidney disease (CKD), <br/>the treatment landscape is changing</h1>
+      `,
+      background: {
+        image: `url(${require("./assets/background.jpg")})`,
+      },
+      timeout: 4000,
+    },
+    {
+      content: `
+        <h1 class="title">Follow a new pathway to <br/>treat anaemia of CKD…</h1>
+      `,
+      background: {
+        image: `url(${require("./assets/background.jpg")})`,
+      },
+      timeout: 4000,
+    },
+    {
+      content: `3`,
+      background: {
+        color: `#007A33`,
+      },
+      timeout: 4000,
+    },
+    {
+      content: `4`,
+      background: {
+        color: `#007A33`,
+      },
+      timeout: 3000,
     },
   ],
 };
